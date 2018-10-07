d370f822c30e5e3b56daa84ed4849a70c916a81a
Faizal <faizalmalik9633@gmail.com>
2018-10-07 21:50:26 +0530

    #004 - Implemented TopStories Api Service Test methods

---
+++ CodingTest/CodingTestTests/APIServiceTests.swift
@@ -0,0 +1,45 @@
+//
+//  CodingTestTests.swift
+//  CodingTestTests
+//
+//  Created by faizal on 07/10/18.
+//  Copyright © 2018 test. All rights reserved.
+//
+
+import XCTest
+@testable import CodingTest
+
+class APIServiceTests: XCTestCase {
+    
+    var sut: APIService?
+    
+    override func setUp() {
+        super.setUp()
+        sut = APIService()
+    }
+    
+    override func tearDown() {
+        sut = nil
+        super.tearDown()
+    }
+    
+    func test_fetch_top_stories() {
+        
+        // Given A apiservice
+        let sut = self.sut!
+        
+        // When fetch top stories
+        let expect = XCTestExpectation(description: "callback")
+        
+        sut.fetchTopStories(complete: { (success, topStories, error) in
+            expect.fulfill()
+            for story in topStories {
+                XCTAssertNotNil(story)
+            }
+            
+        })
+        
+        wait(for: [expect], timeout: 4.1)
+    }
+    
+}
